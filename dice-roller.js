0f7ec4f17100061b3ab1a005c81240850aab9f51
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-20 11:08:33 -0600

    initial commit

---
+++ dice-roller.js
@@ -0,0 +1,17 @@
+const rollDice = function (roll) { 
+  // gives int 1 - 6
+  let max = Math.ceil(6);
+  let min = Math.floor(1); 
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const returnRolls = function(diceRoll) {
+  // add rolls to array
+  const result = [];
+  for (let i = 0; i < diceRoll; i++) {
+    result.push(rollDice(i));
+  }
+  return `Rolled ${diceRoll} dice: ${result.join(', ')}`;
+}
+
+console.log(returnRolls(process.argv[2]));
